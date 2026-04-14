c55c3ca101c0adc6837f5157ef9c90b6134517db
PES2UG23CS451 <pes2ug23cs451@pesu.pes.edu>
2026-04-14 19:30:58 +0530

    updated files

--- CoinFlipGame.sol
+++ CoinFlipGame.sol
@@ -2,12 +2,27 @@
 pragma solidity ^0.8.0;
 
 contract CoinFlipGame {
+
     event GameResult(address player, bool won, uint256 blockNumber);
 
-    function flip(bool playerChoice) public returns (bool won) {
-        bool result = (block.timestamp % 2 == 0);
+    function flip(bool playerChoice) public payable returns (bool won) {
+        require(msg.value > 0, "Send ETH to play");
+
+        bool result = (uint256(
+            keccak256(abi.encodePacked(block.prevrandao, msg.sender))
+        ) % 2 == 0);
+
         won = (playerChoice == result);
+
+        if (won) {
+            require(address(this).balance >= msg.value * 2, "Not enough contract balance");
+
+            (bool sent, ) = payable(msg.sender).call{value: msg.value * 2}("");
+            require(sent, "Failed to send reward");
+        }
+
         emit GameResult(msg.sender, won, block.number);
-        return won;
     }
+
+    receive() external payable {}
 }
--- GameNFT.sol
+++ GameNFT.sol
@@ -2,6 +2,7 @@
 pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/utils/Strings.sol";
 
 contract GameNFT is ERC721 {
     uint256 private _tokenIds;
@@ -11,6 +12,8 @@ contract GameNFT is ERC721 {
     constructor() ERC721("GameCharacter", "GCHAR") {}
 
     function mintCharacter(string memory name) public returns (uint256) {
+        require(bytes(name).length > 0, "Name required");
+
         _tokenIds++;
         uint256 newId = _tokenIds;
 
@@ -22,5 +25,15 @@ contract GameNFT is ERC721 {
 
     function getMyTokenCount(address owner) public view returns (uint256) {
         return balanceOf(owner);
+    }
+
+    function tokenURI(uint256 tokenId) public view override returns (string memory) {
+        require(_ownerOf(tokenId) != address(0), "Token does not exist");
+
+        return string(abi.encodePacked(
+            "https://api.example.com/metadata/",
+            Strings.toString(tokenId),
+            ".json"
+        ));
     }
 }
